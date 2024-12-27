7b4a6572ce644120316abf6c62602bb96e0ed6b6
professionaltaf <altafraza898@gmail.com>
2024-12-27 06:03:24 -0500

    updated code of redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import User from './User'
+
 //import './App.css'
 
 function App() {
   
   return (
-    <div className='app font-medium'>
+     <div className='app font-medium'>
       <User data={{name:"altaf raza", age:"20"}}></User>
     </div>
+    
+      
   )
 }
 
